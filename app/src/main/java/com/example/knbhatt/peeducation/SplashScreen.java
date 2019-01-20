c78d09a9109ba94584b92111fe0daa618e8063d9
Nikita Bhatt <nikitabhatt099@gmail.com>
2019-01-20 02:40:30 -0800

    Initial commit

---
+++ app/src/main/java/com/example/knbhatt/peeducation/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.knbhatt.peeducation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by KN Bhatt on 7/22/2018.
+ */
+public class SplashScreen extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashfile);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }}
